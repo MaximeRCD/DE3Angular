f0d27aac88d17f0335174bf8e8e78f1257a27f64
maxime richaudeau <maxime.richaudeau1@gmail.com>
2022-12-13 18:00:40 +0100

    Add contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UserListComponent} from "./components/user-list/user-list.component";
 import {UserDetailComponent} from "./components/user-detail/user-detail.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
 const routes: Routes = [
   {path: "", component: UserListComponent},
   {path: "users", component:UserListComponent},
   {path: "user-detail", component:UserDetailComponent},
+  {path: "contact", component:ContactComponent},
+
 ];
 
 @NgModule({
