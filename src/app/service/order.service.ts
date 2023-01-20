c8515c1b27af6ab3edaff14f9d42154afaa3ea94
maxime richaudeau <maxime.richaudeau1@gmail.com>
2023-01-20 22:09:19 +0100

    Pretify Order Service

--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -1,3 +1,10 @@
+/*
+* Ce fichier contient la définition du service qui
+* s'occupe de la récupération des commandes depuis
+* le fichier (json) correspondants placé dans le
+* dossier ./src/assets
+*/
+
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Order} from "./orderSchema";
@@ -5,11 +12,13 @@ import {Order} from "./orderSchema";
 @Injectable({
   providedIn: 'root'
 })
+
 export class OrderService {
 
-  orderUrl: string = "../../assets/orders.json"
-  savedOrder!:Order;
-  constructor(private http:HttpClient) { }
+  orderUrl: string = "../../assets/orders.json";
+  savedOrder!: Order;
+
+  constructor(private http:HttpClient) {}
 
   getUser(){
     return this.http.get<Order[]>(this.orderUrl)
