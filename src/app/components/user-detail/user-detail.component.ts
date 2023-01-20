ce64ab8f22f9b9c604e1d335cfa44a27d231317e
maxime richaudeau <maxime.richaudeau1@gmail.com>
2023-01-20 22:29:48 +0100

    Pretify user detail component

--- src/app/components/user-detail/user-detail.component.ts
+++ src/app/components/user-detail/user-detail.component.ts
@@ -1,23 +1,30 @@
-import {Component,Input, OnInit} from '@angular/core';
-import {User} from "../../service/userSchema";
-import {UserService} from "../../service/user.service";
-import {Router} from "@angular/router";
-import {OrderService} from "../../service/order.service";
+/*
+* Ce fichier déclare le composant qui affiche les
+* le détail des informations concernant un utilisateur
+* choisi à partir de la liste des utilisateurs affichés.
+*/
+
+import { Component, OnInit } from '@angular/core';
+import { User } from "../../service/userSchema";
+import { UserService } from "../../service/user.service";
+import { Router } from "@angular/router";
+import { OrderService } from "../../service/order.service";
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css']
 })
+
 export class UserDetailComponent implements OnInit{
+
   userDetail!: User;
   fromOrder: boolean = false;
 
   constructor(
     private orderService: OrderService,
     private userService: UserService,
-              private router: Router) {
-  }
+    private router: Router) {}
 
   ngOnInit(): void {
     this.userDetail = this.userService.savedUser;
